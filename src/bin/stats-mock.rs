801e42d655b6491d449141b46a7a727be1bdce18
Tom Milligan <tommilligan@users.noreply.github.com>
2018-12-09 16:29:48 +0000

    updated stats structures and methods

--- src/bin/stats-mock.rs
+++ src/bin/stats-mock.rs
@@ -1,18 +1,56 @@
 extern crate mcmc_decrypt;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
 
 use std::collections::HashMap;
 use std::io::{self, BufRead};
 
-static SPACE: char = ' ';
+static CHAR_SPACE: char = ' ';
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SymbolStats {
+    frequency: u64,
+    next_symbol: HashMap<char, u64>,
+}
+
+impl SymbolStats {
+    fn new() -> SymbolStats {
+        SymbolStats {
+            frequency: 0,
+            next_symbol: HashMap::new(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct CorpusStats {
+    symbols: HashMap<char, SymbolStats>,
+}
+
+impl CorpusStats {
+    fn new() -> CorpusStats {
+        CorpusStats {
+            symbols: HashMap::new(),
+        }
+    }
+    fn increment(&mut self, symbol: char) -> () {
+        self.symbols
+            .entry(symbol)
+            .or_insert(SymbolStats::new())
+            .frequency += 1;
+    }
+}
 
 fn main() {
-    let mut frequency: HashMap<char, u64> = HashMap::new();
+    let mut stats = CorpusStats::new();
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         for c in line.unwrap().chars() {
-            *frequency.entry(c).or_insert(0) += 1;
+            stats.increment(c)
         }
-        *frequency.entry(SPACE).or_insert(0) += 1;
+        stats.increment(CHAR_SPACE)
     }
-    println!("{:?}", frequency);
+    println!("{}", serde_json::to_string(&stats).unwrap());
 }
